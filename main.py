c361eb716dc4eca4286f9cdaf0b81259b2b1cacf
jackie saltalepre <pirrulez@gmail.com>
2016-12-24 09:59:29 +0000

    levels from file, player spawnpoint

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ pygame.init()
 
 WIDTH = 800
 HEIGHT = 600
+TILESIZE = 40
 
 
 class Ball(pygame.sprite.Sprite):
@@ -73,12 +74,14 @@ class Player(pygame.sprite.Sprite):
         self.image = Player.IMG
         self.rect = self.image.get_rect(topleft=(x, y))
 
+        self.spawnpoint = (x, y)
+
         self.left = False
         self.right = False
         self.up = False
         self.down = False
 
-    def update(self, dt, tiles):
+    def update(self, dt, tiles, balls):
         step = Player.STEP * (dt / 1000.0)
         step = int(step) if step >= 1 else step
 
@@ -110,7 +113,8 @@ class Player(pygame.sprite.Sprite):
             if self.rect.bottom > HEIGHT:
                 self.rect.bottom = HEIGHT
 
-
+        if pygame.sprite.spritecollide(self, balls, False):
+            self.rect.topleft = (self.spawnpoint[0], self.spawnpoint[1])
 
 
 class Tile(pygame.sprite.Sprite):
@@ -132,26 +136,44 @@ class Game:
         pygame.display.set_caption("Ball Game")
 
         self.tiles = pygame.sprite.Group()
-        self.tiles.add(Tile(20, 20))
-        self.tiles.add(Tile(200, 20))
-        self.tiles.add(Tile(450, 480))
-        self.tiles.add(Tile(0, 480))
-        self.tiles.add(Tile(300, 260))
-        self.tiles.add(Tile(160, 340))
-        self.tiles.add(Tile(600, 380))
-        self.tiles.add(Tile(0, 380))
-        self.tiles.add(Tile(80, 500))
-        self.tiles.add(Tile(100, 20))
         self.players = pygame.sprite.Group()
-        self.player1 = Player(300, 30)
-        self.players.add(self.player1)
+        self.player1 = None
+        self.player2 = None
         self.balls = pygame.sprite.Group()
-        self.balls.add(Ball(100, 400))
-        self.balls.add(Ball(300, 500))
 
         self.clock = pygame.time.Clock()
 
+    def setup(self):
+        level_file = "level1.txt"
+        level = open(level_file, "r")
+        lines_list = []
+        for line in level:
+            lines_list.append(line.rstrip("\r\n"))
+        level.close()
+
+        x = 0
+        y = 0
+        for line in lines_list:
+            for char in line:
+                if char == " ":
+                    continue
+                if char == "#":
+                    self.tiles.add(Tile(x, y))
+                if char == "1":
+                    self.player1 = Player(x, y)
+                    self.players.add(self.player1)
+                if char == "O":
+                    self.balls.add(Ball(x, y))
+                x += TILESIZE
+            x = 0
+            y += TILESIZE
+
+
+
+
     def play(self):
+        self.setup()
+
         while True:
             dt = self.clock.tick(Game.FPS)
             for event in pygame.event.get():
@@ -182,7 +204,7 @@ class Game:
 
 
             self.tiles.update(dt)
-            self.players.update(dt, self.tiles)
+            self.players.update(dt, self.tiles, self.balls)
             self.balls.update(dt, self.tiles)
 
             self.screen.blit(self.background, (0, 0))
