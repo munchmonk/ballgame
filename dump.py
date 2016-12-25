3a2c7f42928c52d70a25cbf693eb3b2150c8afda
jackie saltalepre <pirrulez@gmail.com>
2016-12-25 14:40:32 +0000

    invulnerability, removed keyboard

---
+++ dump.py
@@ -0,0 +1,37 @@
+# elif event.type == pygame.KEYDOWN:
+                #     if event.key == pygame.K_w:
+                #         self.player1.up = True
+                #     if event.key == pygame.K_a:
+                #         self.player1.left = True
+                #     if event.key == pygame.K_s:
+                #         self.player1.down = True
+                #     if event.key == pygame.K_d:
+                #         self.player1.right = True
+                #
+                #     if event.key == pygame.K_UP:
+                #         self.player2.up = True
+                #     if event.key == pygame.K_LEFT:
+                #         self.player2.left = True
+                #     if event.key == pygame.K_DOWN:
+                #         self.player2.down = True
+                #     if event.key == pygame.K_RIGHT:
+                #         self.player2.right = True
+                #
+                # elif event.type == pygame.KEYUP:
+                #     if event.key == pygame.K_w:
+                #         self.player1.up = False
+                #     if event.key == pygame.K_a:
+                #         self.player1.left = False
+                #     if event.key == pygame.K_s:
+                #         self.player1.down = False
+                #     if event.key == pygame.K_d:
+                #         self.player1.right = False
+                #
+                #     if event.key == pygame.K_UP:
+                #         self.player2.up = False
+                #     if event.key == pygame.K_LEFT:
+                #         self.player2.left = False
+                #     if event.key == pygame.K_DOWN:
+                #         self.player2.down = False
+                #     if event.key == pygame.K_RIGHT:
+                #         self.player2.right = False
